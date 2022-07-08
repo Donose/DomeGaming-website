cd40b74629fbcf448a2a3eedef818bafdf5e87b9
Andrei Donose <donose-daniel.andrei@outlook.com>
2022-07-08 17:24:56 +0300

    comint this now

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".item-list");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
